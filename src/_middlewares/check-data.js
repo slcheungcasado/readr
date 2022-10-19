5f465435dc565c2eee30eaedc44a25e484b2b0b3
Sheng Leung Cheung Casado <cheungcasado.sl@gmail.com>
2022-10-19 19:29:01 +0800

    username fix

--- src/_middlewares/check-data.js
+++ src/_middlewares/check-data.js
@@ -5,7 +5,9 @@ export function checkData(schema) {
   return async function (req, res, next) {
     try {
       const { body } = req;
+      console.log("body.username", body.username);
       req.verifiedData = await schema.validate(body, validationConfigs);
+      console.log("req.verifiedData.username", req.verifiedData.username);
       return next();
     } catch (err) {
       return handleErrors(res, err);
--- src/schemas/registration.js
+++ src/schemas/registration.js
@@ -27,4 +27,16 @@ export default yup.object({
     .oneOf([yup.ref("password"), null], "Passwords must match")
     .required()
     .label("Password Confirmation"),
+  username: yup.string().label("Username"),
+  avatar: yup
+    .mixed()
+    .test({
+      message: () => "File size is too large (max size: 1 MB)",
+      test: (value) => !value || value?.size < 1024 * 1024,
+    })
+    .test({
+      message: () => "Supported File Formats: .jpg, .jpeg, .png",
+      test: (value) =>
+        !value || (value && SUPPORTED_FORMATS.includes(value.mimetype)),
+    }),
 });
